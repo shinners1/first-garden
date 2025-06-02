ff7c70ce9aede4f27a0a3c2a8ef9d2acaefb4265
shinners1 <49718885+shinners1@users.noreply.github.com>
2025-06-02 17:53:10 +0900

    Update eleventyComputed.js

--- src/site/_data/eleventyComputed.js
+++ src/site/_data/eleventyComputed.js
@@ -3,7 +3,12 @@ const { getFileTree } = require("../../helpers/filetreeUtils");
 const { userComputed } = require("../../helpers/userUtils");
 
 module.exports = {
-  graph: (data) => getGraph(data),
+  // Eleventy가 getGraph의 Promise를 await 하도록 async/await로 감싸줍니다.
+  graph: async (data) => {
+    return await getGraph(data);
+  },
+
+  // getFileTree와 userComputed는 동기 또는 내부에서 자체적으로 Promise를 처리한다면 그대로 두어도 됩니다.
   filetree: (data) => getFileTree(data),
-  userComputed: (data) => userComputed(data)
+  userComputed: (data) => userComputed(data),
 };
